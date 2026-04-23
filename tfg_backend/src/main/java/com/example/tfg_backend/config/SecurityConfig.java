3a3c484d57a6f95b28c8fe787d7a66640a3d526e
davidramost <davidrtornay@gmail.com>
2026-04-23 12:02:37 +0200

    feat(cors): put professional hosting url to cors Coded-by: David R. <davidrtornay@gmail.com>

--- tfg_backend/src/main/java/com/example/tfg_backend/config/SecurityConfig.java
+++ tfg_backend/src/main/java/com/example/tfg_backend/config/SecurityConfig.java
@@ -48,7 +48,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000",
+                "https://davidr.cicloflorenciopintado.es"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
