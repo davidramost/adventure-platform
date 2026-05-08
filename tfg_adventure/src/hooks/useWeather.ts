c3b3ed4f80465143cfef1e62acf91ece2ca949d5
davidramost <davidrtornay@gmail.com>
2026-05-08 13:04:41 +0200

    feat(weather): put more info for weather Coded-by: David R. <davidrtornay@gmail.com>

--- tfg_adventure/src/hooks/useWeather.ts
+++ tfg_adventure/src/hooks/useWeather.ts
@@ -7,6 +7,11 @@ interface WeatherData {
     icon: string;
     humidity: number;
     wind_speed: number;
+    wind_deg: number;
+    wind_gust: number;
+    pressure: number;
+    visibility: number;
+    cloudiness: number;
     city: string;
 }
 
@@ -43,7 +48,12 @@ export function useWeather(lat: number | null, lon: number | null): UseWeatherRe
                     description: data.weather[0].description,
                     icon: data.weather[0].icon,
                     humidity: data.main.humidity,
-                    wind_speed: data.wind.speed,
+                    wind_speed: Math.round(data.wind.speed * 10) / 10,
+                    wind_deg: data.wind.deg || 0,
+                    wind_gust: Math.round((data.wind.gust || 0) * 10) / 10,
+                    pressure: data.main.pressure,
+                    visibility: Math.round(data.visibility / 1000),
+                    cloudiness: data.clouds.all,
                     city: data.name,
                 });
             })
